64ca0162672b9ab2890fee13323096534f7d3e89
Marc Calvet Tomàs <marccalvet10@gmail.com>
2026-05-15 19:37:40 +0200

    FEAT:render

--- DjangoProject_prova/build.sh
+++ DjangoProject_prova/build.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
-python manage.py migrate
-python manage.py collectstatic --no-input
+
+# Sortir immediatament si un comando falla
+set -o errexit
+# Entrar a la carpeta on està el manage.py
+cd DjangoProject_prova
+# Executar les migracions i el collectstatic usant poetry
+poetry run python manage.py migrate
+poetry run python manage.py collectstatic --no-input
